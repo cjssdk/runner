32ad1bcb732ba891d30c9813bb7009cc4b85236e
DarkPark <darkpark@pisem.net>
2016-02-03 00:17:12 +0200

    add draft implementations

--- index.js
+++ index.js
@@ -5,7 +5,9 @@
 
 'use strict';
 
-var Emitter = require('cjs-emitter');
+var Emitter  = require('cjs-emitter'),
+    parallel = require('cjs-async/parallel'),
+    serial   = require('cjs-async/serial');
 
 
 /**
@@ -13,14 +15,13 @@ var Emitter = require('cjs-emitter');
  *
  * @constructor
  * @extends Emitter
- *
- * @param {Object} [data={}] init attributes
  */
-function Runner ( data ) {
+function Runner () {
     // parent constructor call
     Emitter.call(this);
 
-    this.data = data || {};
+    this.tasks = {};
+    this.running = {};
 }
 
 
@@ -36,15 +37,100 @@ Runner.prototype.constructor = Runner;
  *
  * @fires Runner#clear
  */
-Runner.prototype.task = function () {
-
-
-    // there are some listeners
-    if ( this.events['clear'] ) {
-        // notify listeners
-        this.emit('clear', {data: data});
+Runner.prototype.task = function ( name, body ) {
+    if ( name && typeof name === 'string' && body && typeof body === 'function' ) {
+        this.tasks[name] = body;
     }
 
+    // there are some listeners
+    //if ( this.events['clear'] ) {
+    //    // notify listeners
+    //    this.emit('clear', {data: data});
+    //}
+};
+
+
+Runner.prototype.parallel = function () {
+    var self  = this,
+        tasks = Array.prototype.slice.call(arguments);
+
+    tasks = tasks.map(function ( item ) {
+        var name, func;
+
+        if ( typeof item === 'string' ) {
+            name = item;
+            func = self.tasks[item];
+        } else {
+            name = item.name;
+            func = item;
+        }
+
+        return function ( done ) {
+            self.run()
+        };
+    });
+
+    return function ( done ) {
+        parallel(tasks, done);
+    };
+};
+
+
+Runner.prototype.serial = function () {
+    var self  = this,
+        tasks = Array.prototype.slice.call(arguments);
+
+    tasks = tasks.map(function ( item ) {
+        item = typeof item === 'string' ? self.tasks[item] : item;
+
+        return function () {
+
+        };
+    });
+
+    return function ( done ) {
+        serial(tasks, done);
+    };
+};
+
+
+Runner.prototype.run = function ( name ) {
+    var self = this,
+        done = function () {
+            // mark finished
+            self.running[name] = false;
+
+            // there are some listeners
+            if ( self.events['finish'] ) {
+                // notify listeners
+                self.emit('finish', {task: name});
+                console.log('finish', name);
+            }
+        };
+
+    // exist and not already executing
+    if ( this.tasks[name] && !this.running[name] ) {
+        // mark to prevent multiple starts
+        this.running[name] = true;
+
+        // there are some listeners
+        if ( this.events['start'] ) {
+            // notify listeners
+            this.emit('start', {task: name});
+            console.log('start', name);
+        }
+
+        // is there a callback for task?
+        if ( this.tasks[name].length === 0 ) {
+            // start task sync
+            this.tasks[name]();
+            // finish
+            done();
+        } else {
+            // start task async
+            this.tasks[name](done);
+        }
+    }
 };
 
 
