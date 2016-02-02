bc68398f1f3a6b48d978bb4786c9e77878f7f2fd
Stanislav Kalashnik <sk@infomir.eu>
2016-02-02 18:39:32 +0200

    fix draft

--- index.js
+++ index.js
@@ -5,5 +5,48 @@
 
 'use strict';
 
+var Emitter = require('cjs-emitter');
+
+
+/**
+ *
+ *
+ * @constructor
+ * @extends Emitter
+ *
+ * @param {Object} [data={}] init attributes
+ */
+function Runner ( data ) {
+    // parent constructor call
+    Emitter.call(this);
+
+    this.data = data || {};
+}
+
+
+// inheritance
+Runner.prototype = Object.create(Emitter.prototype);
+Runner.prototype.constructor = Runner;
+
+
+/**
+ * Remove all attributes from the model.
+ *
+ * @return {boolean} operation status
+ *
+ * @fires Runner#clear
+ */
+Runner.prototype.task = function () {
+
+
+    // there are some listeners
+    if ( this.events['clear'] ) {
+        // notify listeners
+        this.emit('clear', {data: data});
+    }
+
+};
+
+
 // public
-module.exports = {};
+module.exports = Runner;
