c0a6f65ca1d421c2eda992fa053d7913dda0b91e
Stanislav Kalashnik <s.kalashnik@infomir.com>
2018-06-11 14:34:58 +0300

    rework tasks handling

--- index.js
+++ index.js
@@ -10,9 +10,59 @@ var Emitter  = require('@cjssdk/emitter'),
     serial   = require('@cjssdk/async/serial');
 
 
+function wrap ( instance, taskId, taskBody ) {
+    var time;
+
+    return function ( cb ) {
+        var done = function () {
+                // there are some listeners
+                if ( instance.events['finish'] ) {
+                    // notify listeners
+                    instance.emit('finish', {id: taskId, time: Date.now() - time});
+                }
+
+                if ( cb ) {
+                    console.assert(typeof cb === 'function', 'user callback should be a function');
+
+                    cb.apply(null, arguments);
+                }
+
+                // mark finished
+                taskBody.running = false;
+            },
+            result;
+
+        // exist and not already executing
+        if ( taskBody && !taskBody.running ) {
+            // mark to prevent multiple starts
+            taskBody.running = true;
+
+            time = Date.now();
+
+            // there are some listeners
+            if ( instance.events['start'] ) {
+                // notify listeners
+                instance.emit('start', {id: taskId});
+            }
+
+            // is there a callback for task?
+            if ( taskBody.length === 0 ) {
+                // start task sync
+                result = taskBody();
+                // finish
+                done(result === false);
+            } else {
+                // start task async
+                result = taskBody(done);
+            }
+        }
+
+        return result;
+    };
+}
+
+
 /**
- *
- *
  * @constructor
  * @extends Emitter
  */
@@ -23,8 +73,6 @@ function Runner () {
     Emitter.call(this);
 
     this.tasks = {};
-    this.tree  = {};
-    //this.running = {};
 }
 
 
@@ -36,7 +84,7 @@ Runner.prototype.constructor = Runner;
 /**
  * Create a task with the given identifier.
  *
- * @param {string} id task name
+ * @param {string} id task unique name
  * @param {function} body task method
  *
  * @return {function} task
@@ -44,222 +92,92 @@ Runner.prototype.constructor = Runner;
  * //@fires Runner#clear
  */
 Runner.prototype.task = function ( id, body ) {
-    if ( id && typeof id === 'string' && body && typeof body === 'function' ) {
-        this.tasks[id] = body;
-        body.id = id;
-        //console.log(body);
-    }
+    console.assert(arguments.length === 2, 'wrong arguments number');
+    console.assert(typeof id === 'string', 'task id should be a string');
+    console.assert(id.length > 0, 'empty id');
+    console.assert(typeof body === 'function', 'task body should be a function');
+
+    this.tasks[id] = body;
 
     return body;
 };
 
 
-/**
- * Create tasks.
- */
-Runner.prototype.tasks = function () {
-    var index, taskSet, name;
-
-    // walk through all the given task sets
-    for ( index = 0; index < arguments.length; index++ ) {
-        taskSet = arguments[index];
-
-        if ( taskSet && typeof taskSet === 'object' ) {
-            for ( name in taskSet ) {
-                this.task(name, taskSet[name]);
-            }
-        }
-    }
-};
-
-
-//function done ( instance, fn ) {
-//    // mark finished
-//    fn.running = false;
-//
-//    // there are some listeners
-//    if ( instance.events['finish'] ) {
-//        // notify listeners
-//        instance.emit('finish', {id: fn.id});
-//        //console.log('finish', fn.id);
-//    }
-//}
-//
-//function wrap ( instance, fn ) {
-//    // is there a callback for task?
-//    if ( fn.length === 0 ) {
-//        // start task sync
-//        fn();
-//        // finish
-//        done();
-//    } else {
-//        // start task async
-//        fn(done);
-//    }
-//}
-
-
-Runner.prototype.wrap = function ( task ) {
-    var self = this,
-        time;
-
-    return function ( cb ) {
-        var done = function () {
-                // mark finished
-                task.running = false;
-
-                time = Date.now() - time;
-                /*console.log('finish', task.id, time);/**/
-
-                // there are some listeners
-                if ( self.events['finish'] ) {
-                    // notify listeners
-                    self.emit('finish', {id: task.id, time: time});
-                }
-
-                // callback from user or
-                if ( cb && typeof cb === 'function' ) {
-                    cb.apply(null, arguments);
-                }
-            },
-            result;
-
-        // exist and not already executing
-        if ( task && !task.running ) {
-            // mark to prevent multiple starts
-            task.running = true;
-
-            time = Date.now();
-            /*console.log('start', task.id);/**/
-
-            // there are some listeners
-            if ( self.events['start'] ) {
-                // notify listeners
-                self.emit('start', {id: task.id});
-            }
-
-            // is there a callback for task?
-            if ( task.length === 0 ) {
-                // start task sync
-                result = task();
-                // finish
-                done();
-            } else {
-                //console.log(task.id);
-                // start task async
-                result = task(done);
-            }
-        }
-
-        return result;
-    };
-};
-
-
+// accepts list of task ids
 Runner.prototype.parallel = function () {
     var self  = this,
-        tasks = Array.prototype.slice.call(arguments),
-        func;
+        tasks = Array.prototype.slice.call(arguments);
 
+    // no tasks were given
     if ( tasks.length === 0 ) {
         return null;
     }
 
-    func = function ( done ) {
-        var readyTasks = [];
-
-        // wrap not running tasks only
-        tasks.forEach(function ( task ) {
-            task = self.tasks[task] || task;
-
-            if ( task && !task.running ) {
-                readyTasks.push(
-                    function ( done ) {
-                        return self.run(task, done);
-                    }
-                );
-            }
-        });
-
-        parallel(readyTasks, done);
+    return function ( done ) {
+        parallel(tasks.map(function ( taskId ) {
+            return function ( done ) {
+                return self.run(taskId, done);
+            };
+        }), done);
     };
-
-    func.group    = true;
-    func.type     = 'parallel';
-    func.children = tasks;
-
-    return func;
 };
 
 
+// accepts list of task ids
 Runner.prototype.serial = function () {
     var self  = this,
-        tasks = Array.prototype.slice.call(arguments),
-        func;
+        tasks = Array.prototype.slice.call(arguments);
 
+    // no tasks were given
     if ( tasks.length === 0 ) {
         return null;
     }
 
-    func = function ( done ) {
-        var readyTasks = [];
-
-        // wrap not running tasks only
-        tasks.forEach(function ( task ) {
-            task = self.tasks[task] || task;
-
-            if ( task && !task.running ) {
-                readyTasks.push(
-                    function ( done ) {
-                        return self.run(task, done);
-                    }
-                );
-            }
-        });
-
-        serial(readyTasks, done);
+    return function ( done ) {
+        serial(tasks.map(function ( taskId ) {
+            return function ( done ) {
+                return self.run(taskId, done);
+            };
+        }), done);
     };
-
-    func.group    = true;
-    func.type     = 'serial';
-    func.children = tasks;
-
-    return func;
 };
 
 
 /**
  * Start task execution.
  *
- * @param {function|string} task task to run
+ * @param {function|string} taskId task to run
  * @param {function} [done] callback on task finish
  *
- * @return {*} task execution result
+ * @return {boolean} is task really started
  */
-Runner.prototype.run = function ( task, done ) {
-    var result;
+Runner.prototype.run = function ( taskId, done ) {
+    var started = false,
+        taskBody;
 
-    // lookup
-    task = this.tasks[task] || task;
+    console.assert(arguments.length >= 1, 'wrong arguments number');
+    console.assert(typeof taskId === 'string', 'taskId should be a string');
+    console.assert(taskId.length > 0, 'empty taskId');
+    console.assert(arguments.length === 1 || typeof done === 'function', 'done should be a function');
 
-    if ( typeof task === 'function' ) {
-        // task was found but
-        // may be executing at the moment
-        if ( !task.running ) {
-            result = this.wrap(task)(done);
+    taskBody = this.tasks[taskId];
+
+    if ( typeof taskBody === 'function' ) {
+        if ( !taskBody.running ) {
+            wrap(this, taskId, taskBody)(done);
+
+            started = true;
         }
-    } else if ( this.events['error'] ) {
-        // task was not found
-        // so notify listeners
-        this.emit('error', {id: task, code: 404});
+    } else {
+        this.emit('error', {id: taskId, code: 404});
     }
 
-    return result;
+    return started;
 };
 
 
 Runner.prototype.start = function ( done ) {
-    this.run(this.tasks.default, done);
+    this.run('default', done);
 };
 
 
